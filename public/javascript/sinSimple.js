a079fbb72aa2444a81bf5e97f5161abe4d7a8188
Faye Purdum <fayepurdum@gmail.com>
2013-04-15 18:06:44 -0700

    bootstrap and conditionals

--- public/javascript/sinSimple.js
+++ public/javascript/sinSimple.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
     $.ajax({
-      url : "http://api.wunderground.com/api/"yourKeyHere"/geolookup/conditions/q/OR/Portland.json",
+      url : "http://api.wunderground.com/api/2b0bc452f77b4610/geolookup/conditions/q/OR/Portland.json",
       dataType : "jsonp",
       success : function(parsed_json) {
         var location = parsed_json['location']['city'];
         var temp_f = parsed_json['current_observation']['temp_f'];
         // alert("Current temperature in " + location + " is: " + temp_f);
-        $("#container").append("<div>Hi " + location + "! It's currently " + temp_f + " outside.</div>");
+        $(".hero-unit").append("<div>Hi, " + location + "! It's currently " + temp_f + " outside.</div>");
         if (temp_f<50)
         {
-          $("#container").append("<div>Wow! It's cold!</div>");
+          $(".hero-unit").append("<div>Wow! It's cold!</div>");
         }
         else
         {
-          $("#container").append("<div>What are you complaining about?</div>");
+          $(".hero-unit").append("<div>What are you complaining about?</div>");
         }
       }
     });
@@ -21,11 +21,25 @@ $(document).ready(function() {
       url: "http://api.wunderground.com/api/2b0bc452f77b4610/forecast10day/q/OR/Portland.json",
       dataType: "jsonp",
       success : function(parsed_json) {
+        // for each weather class do?
         for (var i = 0; i <= 9; i++) {
           //var conditions = parsed_json['forecast']['simpleforecast']['forecastday'][i]['conditions'];
           var conditions = parsed_json.forecast.simpleforecast.forecastday[i].conditions;
           var day = parsed_json.forecast.simpleforecast.forecastday[i].date.weekday;
-          $("#box").append("<div>" + day + " : " + conditions + "</div>");
+          var wind = parsed_json.forecast.simpleforecast.forecastday[i].avewind.mph;
+          var low = parsed_json.forecast.simpleforecast.forecastday[i].low.fahrenheit;
+          var high = parsed_json.forecast.simpleforecast.forecastday[i].high.fahrenheit;
+          var rain = parsed_json.forecast.simpleforecast.forecastday[i].qpf_day.in;
+          $(".day" + i).append("<h2>" + day + ": " + conditions + " and a high of " + high + " degrees</h2>");
+          if (wind>5) {
+            $(".day" + i).append("<h2>It's too windy to ride today! " + wind + " mph!</h2>");
+          };
+          if (low<37) {
+            $(".day" + i).append("<h2>It's too cold to ride today! The low is " + low + " degrees!</h2>");
+          };
+          if (rain>.006) {
+            $(".day" + i).append("<h2>It's wet out there. Wear some waterproof!</h2>");
+          };
         console.log(parsed_json);
         };
       }
